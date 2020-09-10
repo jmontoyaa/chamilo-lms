7509bb433a24d1a193d070fda00bc2c3820b4ac7
Julio Montoya <gugli100@gmail.com>
2020-09-10 08:44:30 +0200

    Update setup-apache.sh

--- tests/travis/setup-apache.sh
+++ tests/travis/setup-apache.sh
@@ -2,8 +2,6 @@
 
 echo "* Start setup-apache.sh ...";
 
-sudo a2enmod rewrite actions fastcgi alias
-
 # Use default config
 sudo cp -f tests/travis/travis-apache /etc/apache2/sites-available/000-default.conf
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/000-default.conf
